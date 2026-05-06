2b09b6a100303c6fb35200a25c7705be99d0ed58
versusfor <versusforever@gmail.com>
2026-05-06 03:28:44 -0400

    main.py
    Updated Telegram token and chat ID for messaging.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import datetime
 
 # 🔐 PEGA AQUÍ TUS CREDENCIALES NUEVAS (NO LAS COMPARTAS)
-TELEGRAM_TOKEN = "8755288854:AAHRCCsk2CHmLLHDLnXYUEHK6ZWAywSVnGo"
-CHAT_ID = "8755288854"  # Ej: 123456789
+TELEGRAM_TOKEN = "8774099880:AAFyVd-Id8eC4sGVztg9W0Z-1kPstLu6Hb0"
+CHAT_ID = "8774099880"  # Ej: 123456789
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
